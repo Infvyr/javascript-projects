db26b1e1e537c87dd50c8fb7fe782ed6be94bd11
Infvyr <allfortrashmail@gmail.com>
2021-03-28 21:16:58 +0300

    updated users.json and users.js

---
+++ js-ecommerce-simple/routes/admin/auth.js
@@ -0,0 +1,90 @@
+const express = require("express");
+
+// destructuring check function from express-validator
+// instead of using const expressValidator = ...
+const { check, validationResult } = require("express-validator");
+
+const usersRepo = require("../../repositories/users");
+const signupTemplate = require("../../views/admin/auth/signup");
+const signinTemplate = require("../../views/admin/auth/signin");
+const {
+    requireEmail,
+    requirePassword,
+    requirePasswordConfirmation,
+} = require("./validators");
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+    res.send(signupTemplate({ req }));
+});
+
+// express middleware function
+// const bodyParser = (req, res, next) => {
+//     if (req.method == "POST") {
+//         // get access to email, password, passwordConfirmation
+//         // data == event
+//         req.on("data", (data) => {
+//             const parsed = data.toString("utf8").split("&");
+//             const formData = {};
+//             for (let pair of parsed) {
+//                 const [key, value] = pair.split("=");
+//                 formData[key] = value;
+//             }
+//             req.body = formData;
+//             next();
+//         });
+//     } else {
+//         next();
+//     }
+// };
+
+// urlencoded = same thing as the code commented above
+router.post(
+    "/signup",
+    [requireEmail, requirePassword, requirePasswordConfirmation],
+    async (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        const { email, password, passwordConfirmation } = req.body;
+
+        // Create a user in our repo to represent this person
+        const user = await usersRepo.create({
+            email: email,
+            password: password,
+        });
+
+        // Store the id of that user inside the users cookies
+        // req.session === {}; // Added by cookie session!
+        req.session.userId = user.id;
+
+        res.send("Account created!!!");
+    },
+);
+
+router.get("/signout", (req, res) => {
+    req.session = null;
+    res.send("You are logged out!");
+});
+
+router.get("/signin", (req, res) => {
+    res.send(signinTemplate());
+});
+
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({ email: email });
+
+    if (!user) return res.send("Email not found");
+
+    const validPassword = await usersRepo.comparePasswords(
+        user.password,
+        password, // req.body
+    );
+    if (!validPassword) return res.send("Invalid password!");
+
+    req.session.userId = user.id;
+    res.send("You are signed in!!!");
+});
+
+module.exports = router;
---
+++ js-ecommerce-simple/views/admin/auth/signup.js
@@ -0,0 +1,16 @@
+const layout = require("../layout");
+
+module.exports = ({ req }) => {
+    return layout({
+        content: `
+        <div>
+            Your id is: ${req.session.userId}
+            <form method="POST">
+                <input placeholder="email" name="email" />
+                <input placeholder="password" name="password" />
+                <input placeholder="password confirmation" name="passwordConfirmation" />
+                <button>Sign up</button>
+            </form>
+        </div>`,
+    });
+};
