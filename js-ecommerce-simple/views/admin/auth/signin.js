c1cee75a2b8243fd68dee2bf5706c86dfb2cccb4
Infvyr <allfortrashmail@gmail.com>
2021-03-29 22:17:29 +0300

    added new layout

--- js-ecommerce-simple/views/admin/auth/signin.js
+++ js-ecommerce-simple/views/admin/auth/signin.js
@@ -1,14 +1,36 @@
 const layout = require("../layout");
+const { getError } = require("../../helpers");
 
-module.exports = () => {
+module.exports = ({ errors }) => {
     return layout({
         content: `
-        <div>
-            <form method="POST">
-                <input placeholder="email" name="email" />
-                <input placeholder="password" name="password" />
-                <button>Sign In</button>
-            </form>
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-one-quarter">
+                    <form method="POST">
+                    <h1 class="title">Sign in</h1>
+                    <div class="field">
+                        <label for="email" class="label">Email</label>
+                        <input placeholder="email" name="email" id="email" class="input" required/>
+                        <p class="help is-danger">${getError(
+                            errors,
+                            "email",
+                        )}</p>
+                    </div>
+                    <div class="field">
+                        <label for="password" class="label">Password</label>
+                        <input placeholder="password" name="password" id="password" class="input" required/>
+                        <p class="help is-danger">${getError(
+                            errors,
+                            "password",
+                        )}</p>
+                    </div>
+                    <button class="button is-primary">Submit</button>
+                    </form>
+                    <hr>
+                    <a href="/signup">Need an account? Sign up</a>
+                </div>
+            </div>
         </div>`,
     });
 };
