05aa0d192e12099206bd6aa1feab9ff0ab746345
Infvyr <allfortrashmail@gmail.com>
2021-04-20 21:30:43 +0300

    update create new product form

--- js-ecommerce-simple/views/admin/products/new.js
+++ js-ecommerce-simple/views/admin/products/new.js
@@ -1,15 +1,38 @@
 const layout = require("../layout");
 const { getError } = require("../../helpers");
 
-module.exports = ({ error }) => {
+module.exports = ({ errors }) => {
     return layout({
         content: `
-            <form method="POST">
-                <input type="text" name="title" placeholder="Title" />
-                <input type="text" name="price" placeholder="Price" />
-                <input type="file" name="image" />
-                <button>Add new product</button>
-            </form>
+            <div class="columns is-centered">
+                <div class="column is-half">
+                    <h1 class="subtitle">Create a product</h1>
+                    <form method="POST" enctype="multipart/form-data" class="new-product">
+                        <div class="field">
+                            <label for="title" class="label">Title</label>
+                            <input type="text" id="title" class="input" name="title" placeholder="Title" />
+                            <p class="help is-danger">${getError(
+                                errors,
+                                "title",
+                            )}</p>
+                        </div>
+                        <div class="field">
+                            <label for="price" class="label">Price</label>
+                            <input type="text" id="price" class="input" name="price" placeholder="Price" />
+                            <p class="help is-danger">${getError(
+                                errors,
+                                "price",
+                            )}</p>
+                        </div>
+                        <div class="field">
+                            <label for="image" class="label">Image</label>
+                            <input type="file" id="image" name="image" />
+                        </div>
+                        </br>
+                        <button class="button is-primary">Add new product</button>
+                    </form>
+                </div>
+            </div>
         `,
     });
 };
