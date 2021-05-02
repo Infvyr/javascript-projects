03bc27086d16e7755f5a7712f3a43a9ccd9879d0
Infvyr <allfortrashmail@gmail.com>
2021-05-02 20:16:49 +0300

    Create autocomplete helper function

---
+++ search-films/autocomplete.js
@@ -0,0 +1,63 @@
+const createAutoComplete = ({
+    fetchData,
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+}) => {
+    // const root = document.querySelector(".autocomplete-films");
+    root.innerHTML = `
+    <label><b>Search:</b></label>
+    <input class="input search-input" type="search" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+    const input = root.querySelector(".search-input");
+    const dropwdown = root.querySelector(".dropdown");
+    const resultWrapper = root.querySelector(".results");
+    const isActiveClass = "is-active";
+
+    const onInput = async (e) => {
+        const items = await fetchData(e.target.value);
+
+        // close dropdown when input is blank
+        if (!items.length) {
+            dropwdown.classList.remove("is-active");
+            return;
+        }
+
+        resultWrapper.innerHTML = "";
+        dropwdown.classList.add(isActiveClass);
+
+        for (let item of items) {
+            const option = document.createElement("a");
+
+            option.classList.add("dropdown-item");
+
+            option.innerHTML = renderOption(item);
+
+            // detect the click on individual option
+            // and fill the search input out with movie title
+            option.addEventListener("click", () => {
+                dropwdown.classList.remove(isActiveClass);
+                input.value = inputValue(item);
+                onOptionSelect(item);
+            });
+
+            resultWrapper.appendChild(option);
+        }
+    };
+
+    input.addEventListener("input", debounce(onInput, 600));
+
+    // close dropdown by clicking outside that
+    document.addEventListener("click", (e) => {
+        if (!root.contains(e.target)) {
+            dropwdown.classList.remove(isActiveClass);
+        }
+    });
+};
